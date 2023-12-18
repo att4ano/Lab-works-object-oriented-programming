1e24c1e9e92e94fa75f36bf89c838f1f67228f35
dubstephf <113085241+dubstephf@users.noreply.github.com>
2023-12-18 23:01:27 +0300

    Delete Lab5.Application directory

--- Lab5.Application/CurrentUserManager.cs
+++
@@ -1,6 +0,0 @@
-namespace Lab5.Application;
-
-public class CurrentUserManager : ICurrentUserManager
-{
-    public CurrentSession? CurrentSession { get; set; }
-}
--- Lab5.Application/ICurrentUserManager.cs
+++
@@ -1,6 +0,0 @@
-namespace Lab5.Application;
-
-public interface ICurrentUserManager
-{
-    public CurrentSession? CurrentSession { get; set; }
-}
